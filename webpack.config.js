eeeb2c2a8737b67376240842c532d2ae25caf146
Jim Greenleaf <james.a.greenleaf@gmail.com>
2017-08-18 17:38:10 -0400

    configuration, minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: (isProd ? 'bundle.min.js' : 'bundle.js'),
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
         contentBase: './dist',
-    }
+    },
+    plugins: [],
 };
+
+if (isProd) {
+    module.exports.plugins.push(new MinifyPlugin());
+}
